c2614da87fda0202783210a6e074f2e9df2087c2
Collin Sakuma <collinmsakuma@gmail.com>
2023-02-12 20:13:35 -0800

    test fetch

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-const variable = gitTest
+const randomCocktailUrl = "http://www.thecocktaildb.com/api/json/v1/1/random.php"
+
+fetch(randomCocktailUrl)
+    .then(res => res.json())
+    .then((randomDrink) => addRandomDrink(randomDrink))
+
+function addRandomDrink(randomDrink) {
+    console.log(randomDrink)
+    const main = document.querySelector('main')
+    const img = document.createElement('img')
+    // console.log(randomDrink.drinks[0].strDrink)
+    img.src = randomDrink.drinks[0].strDrinkThumb
+    main.append(img)
+}
