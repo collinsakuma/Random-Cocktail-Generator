e7ef15d5e154ca768e17b81a898e3fbd29a0fc5f
Collin Sakuma <collinmsakuma@gmail.com>
2023-02-13 08:15:19 -0800

    render 5 random cocktails

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
 const randomCocktailUrl = "http://www.thecocktaildb.com/api/json/v1/1/random.php"
+function renderCocktails() {
+    fetch(randomCocktailUrl)
+        .then(res => res.json())
+        .then(randomDrink => addRandomDrink(randomDrink))
+}
 
-fetch(randomCocktailUrl)
-    .then(res => res.json())
-    .then((randomDrink) => addRandomDrink(randomDrink))
 
 function addRandomDrink(randomDrink) {
-    console.log(randomDrink)
     const main = document.querySelector('main')
     const img = document.createElement('img')
-    // console.log(randomDrink.drinks[0].strDrink)
+    img.addEventListener('click', () => console.log('click'))
+    const h4 = document.createElement('h4')
+    h4.textContent = randomDrink.drinks[0].strDrink
     img.src = randomDrink.drinks[0].strDrinkThumb
-    main.append(img)
+    main.append(img, h4)
+}
+
+// render 5 random cocktails //
+for (i=0; i < 5; i++) {
+    renderCocktails()
 }
